adf8fd1a55475876da77520aaa4ebea047f88bb6
juciely <jussi.jpg@outlook.com.br>
2024-08-04 20:51:02 -0300

    Ajuste de iniciação do spring boot

---
+++ src/main/java/com/example/api_eventos_usuarios/ApiEventosUsuariosApplication.java
@@ -0,0 +1,13 @@
+package com.example.api_eventos_usuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiEventosUsuariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiEventosUsuariosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/api_eventos_usuarios/ApiEventosUsuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.api_eventos_usuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiEventosUsuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
